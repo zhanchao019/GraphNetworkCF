ebedb206a8a2c1188f0ce13979c4c264fa6cc204
zhanchao019 <34484847+zhanchao019@users.noreply.github.com>
2021-04-17 22:50:36 +0800

    完成处理数据部分

--- Utils/loadedData.py
+++ Utils/loadedData.py
@@ -1,7 +1,9 @@
-import numpy as np
 import random as rd
-import scipy.sparse as sp
 from time import time
+
+import numpy as np
+import scipy.sparse as sp
+
 
 class Data(object):
     def __init__(self, path, batch_size):
@@ -10,8 +12,8 @@ class Data(object):
 
         train_file = path + '/train.txt'
         test_file = path + '/test.txt'
-        print("train file"+train_file)
-        print("test file"+test_file)
+        print("train file" + train_file)
+        print("test file" + test_file)
 
         #get number of users and items
         self.n_users, self.n_items = 0, 0
@@ -78,5 +80,112 @@ class Data(object):
     def print_statistics(self):
         print('n_users=%d, n_items=%d' % (self.n_users, self.n_items))
         print('n_interactions=%d' % (self.n_train + self.n_test))
-        print('n_train=%d, n_test=%d, sparsity=%.5f' % (self.n_train, self.n_test, (self.n_train + self.n_test)/(self.n_users * self.n_items)))
+        print('n_train=%d, n_test=%d, sparsity=%.5f' % (
+        self.n_train, self.n_test, (self.n_train + self.n_test) / (self.n_users * self.n_items)))
 
+    def get_adj_mat(self):
+        try:
+            t1 = time()
+            adj_mat = sp.load_npz(self.path + '/s_adj_mat.npz')
+            norm_adj_mat = sp.load_npz(self.path + '/s_norm_adj_mat.npz')
+            mean_adj_mat = sp.load_npz(self.path + '/s_mean_adj_mat.npz')
+            print('already load adj matrix', adj_mat.shape, time() - t1)
+
+        except Exception:
+            adj_mat, norm_adj_mat, mean_adj_mat = self.create_adj_mat()
+            sp.save_npz(self.path + '/s_adj_mat.npz', adj_mat)
+            sp.save_npz(self.path + '/s_norm_adj_mat.npz', norm_adj_mat)
+            sp.save_npz(self.path + '/s_mean_adj_mat.npz', mean_adj_mat)
+        return adj_mat, norm_adj_mat, mean_adj_mat
+
+    def create_adj_mat(self):
+        t1 = time()
+        adj_mat = sp.dok_matrix((self.n_users + self.n_items, self.n_users + self.n_items), dtype=np.float32)
+        adj_mat = adj_mat.tolil()
+        R = self.R.tolil()
+
+        adj_mat[:self.n_users, self.n_users:] = R
+        adj_mat[self.n_users:, :self.n_users] = R.T
+        adj_mat = adj_mat.todok()
+        print('already create adjacency matrix', adj_mat.shape, time() - t1)
+
+        t2 = time()
+
+        def normalized_adj_single(adj):
+            rowsum = np.array(adj.sum(1))
+
+            d_inv = np.power(rowsum, -1).flatten()
+            d_inv[np.isinf(d_inv)] = 0.
+            d_mat_inv = sp.diags(d_inv)
+
+            norm_adj = d_mat_inv.dot(adj)
+            # norm_adj = adj.dot(d_mat_inv)
+            print('generate single-normalized adjacency matrix.')
+            return norm_adj.tocoo()
+
+        def check_adj_if_equal(adj):
+            dense_A = np.array(adj.todense())
+            degree = np.sum(dense_A, axis=1, keepdims=False)
+
+            temp = np.dot(np.diag(np.power(degree, -1)), dense_A)
+            print('check normalized adjacency matrix whether equal to this laplacian matrix.')
+            return temp
+
+        norm_adj_mat = normalized_adj_single(adj_mat + sp.eye(adj_mat.shape[0]))
+        mean_adj_mat = normalized_adj_single(adj_mat)
+
+        print('already normalize adjacency matrix', time() - t2)
+        return adj_mat.tocsr(), norm_adj_mat.tocsr(), mean_adj_mat.tocsr()
+
+    def get_num_users_items(self):
+        return self.n_users, self.n_items
+
+    def negative_pool(self):
+        t1 = time()
+        for u in self.train_items.keys():
+            neg_items = list(set(range(self.n_items)) - set(self.train_items[u]))
+            pools = [rd.choice(neg_items) for _ in range(100)]
+            self.neg_pools[u] = pools
+        print('refresh negative pools', time() - t1)
+
+    def get_trainNum(self):
+        return self.n_train
+
+    def sample(self):
+        if self.batch_size <= self.n_users:
+            users = rd.sample(self.exist_users, self.batch_size)
+        else:
+            users = [rd.choice(self.exist_users) for _ in range(self.batch_size)]
+
+        def sample_pos_items_for_u(u, num):
+            pos_items = self.train_items[u]
+            n_pos_items = len(pos_items)
+            pos_batch = []
+            while True:
+                if len(pos_batch) == num: break
+                pos_id = np.random.randint(low=0, high=n_pos_items, size=1)[0]
+                pos_i_id = pos_items[pos_id]
+
+                if pos_i_id not in pos_batch:
+                    pos_batch.append(pos_i_id)
+            return pos_batch
+
+        def sample_neg_items_for_u(u, num):
+            neg_items = []
+            while True:
+                if len(neg_items) == num: break
+                neg_id = np.random.randint(low=0, high=self.n_items, size=1)[0]
+                if neg_id not in self.train_items[u] and neg_id not in neg_items:
+                    neg_items.append(neg_id)
+            return neg_items
+
+        def sample_neg_items_for_u_from_pools(u, num):
+            neg_items = list(set(self.neg_pools[u]) - set(self.train_items[u]))
+            return rd.sample(neg_items, num)
+
+        pos_items, neg_items = [], []
+        for u in users:
+            pos_items += sample_pos_items_for_u(u, 1)
+            neg_items += sample_neg_items_for_u(u, 1)
+
+        return users, pos_items, neg_items

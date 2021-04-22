837a23d5d1a901bd704f97c30203aa6bc7993d09
zhanchao019 <34484847+zhanchao019@users.noreply.github.com>
2021-04-22 18:54:17 +0800

    修正参数解析错误的bug
    Signed-off-by: zhanchao019 <34484847+zhanchao019@users.noreply.github.com>

--- parser.py
+++ parser.py
@@ -22,10 +22,10 @@ def parse_args():
 
     parser.add_argument('--embed_size', type=int, default=64,
                         help='Embedding size.')
-    parser.add_argument('--layer_size', nargs='?', default='[64,64,64]',
+    parser.add_argument('--layer_size', nargs='?', default=[64, 64, 64],
                         help='Output sizes of every layer')
 
-    parser.add_argument('--regs', nargs='?', default='[1e-5]',
+    parser.add_argument('--regs', type=float, default=1e-5,
                         help='Regularizations.')
     parser.add_argument('--lr', type=float, default=0.0001,
                         help='Learning rate.')
@@ -36,7 +36,7 @@ def parse_args():
                         help='0: Disable node dropout, 1: Activate node dropout')
     parser.add_argument('--node_dropout', nargs='?', default='[0.1]',
                         help='Keep probability w.r.t. node dropout (i.e., 1-dropout_ratio) for each deep layer. 1: no dropout.')
-    parser.add_argument('--mess_dropout', nargs='?', default='[0.1,0.1,0.1]',
+    parser.add_argument('--mess_dropout', nargs='?', default=[0.1, 0.1, 0.1],
                         help='Keep probability w.r.t. message dropout (i.e., 1-dropout_ratio) for each deep layer. 1: no dropout.')
 
     parser.add_argument('--Ks', nargs='?', default='[20, 40, 60, 80, 100]',
